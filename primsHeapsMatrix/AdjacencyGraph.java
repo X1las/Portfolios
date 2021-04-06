6eb5a4afe4ae7ebf42226881ed2eee16773a1291
KataRiegels <KataRiegels@users.noreply.github.com>
2021-04-06 10:09:25 +0200

    af

--- primsHeapsMatrix/AdjacencyGraph.java
+++ primsHeapsMatrix/AdjacencyGraph.java
@@ -3,16 +3,14 @@ import java.util.ArrayList;
 import javax.xml.transform.Source;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
 public class AdjacencyGraph {
     ArrayList<Vertex> vertices;
-    ArrayList<MSTNode> mstNodes = new ArrayList<>();
 
-
-    HashMap<Vertex, Integer> vertexMap= new HashMap<>();
 
 
     public AdjacencyGraph() {
@@ -22,18 +20,15 @@ public class AdjacencyGraph {
     public void addVertex(Vertex v) {
         vertices.add(v);
     }
-
-    public ArrayList<Vertex> getVertices() {
-        return this.vertices;
-    }
-
-
+    // cats r cute that's true tho
+    
     public void MSTPrims(){
-        MinHeap<Pair> minheap = new MinHeap<>();
-        HashMap<Vertex,Pair> pairs = new HashMap<>();
+        ArrayList<MSTNode>   mstNodes = new ArrayList<>();      // 1
+        MinHeap<Pair>        minheap  = new MinHeap<>();        // 1
+        HashMap<Vertex,Pair> pairs    = new HashMap<>();        // 1
         for (int i = 0; i < vertices.size(); i++){ //            V log V
             Vertex currentVertex = vertices.get(i);
-            Pair pair = new Pair(currentVertex, currentVertex.getDist());
+            Pair pair = new Pair(currentVertex, currentVertex.dist);
             pair.setVertex(currentVertex);
             pairs.put(currentVertex, pair);
             minheap.insert(pair);
@@ -41,18 +36,18 @@ public class AdjacencyGraph {
     
         Pair startNode = minheap.getMinHeap().get(0);
         startNode.setDistance(0);
-        startNode.getVertex().setPred(startNode.getVertex());
-        while(!minheap.isEmpty()){
-            Pair extractedNode = minheap.extractMin();
-            Vertex extractedVertex = extractedNode.getVertex();
-            MSTNode mstNode = new MSTNode(extractedVertex, extractedVertex.getPred(), extractedNode.distance);
-            mstNodes.add(mstNode);
-            for(Edge edge : extractedVertex.OutEdges){
+        startNode.vertex.setPred(startNode.vertex);
+        while(!minheap.isEmpty()){ 
+            Pair    extractedNode = minheap.extractMin();   // O(v + v²)
+            Vertex  extractedVertex = extractedNode.vertex; // O(1)
+            MSTNode mstNode = new MSTNode(extractedVertex, extractedVertex.pred, extractedNode.distance); // O(1)
+            mstNodes.add(mstNode); // O(v)
+            for(Edge edge : extractedVertex.OutEdges){  // O(e)
                 if (edge.weight < edge.to.dist){
-                    edge.to.setDist(edge.weight);
-                    edge.to.setPred(edge.from);
-                    Pair currentPair = pairs.get(edge.to);
-                    currentPair.setDistance(edge.to.getDist());
+                    edge.to.setDist(edge.weight);   // O(1)
+                    edge.to.setPred(edge.from);     // O(1)
+                    Pair currentPair = pairs.get(edge.to); 
+                    currentPair.setDistance(edge.to.dist);
                     int pos = minheap.getPosition(currentPair);
                     minheap.decreasekey(pos);
                 }
@@ -66,17 +61,59 @@ public class AdjacencyGraph {
         }
         System.out.println("total distance: " + total + " km");
         System.out.printf(Locale.US, "total price:    %,d kroner %n", 100000*total);
-        
     }
 
 
     public void addEdge(Vertex f, Vertex t, Integer w) {
         if (!(vertices.contains(f) && vertices.contains(t))) {
-
             System.out.println(" Vertex not in graph");
             return;
         }
         Edge e = new Edge(f, t, w);
+    }
+
+    public void createFromFile(String file) {
+        String line    = "";
+        String splitBy = ",";
+        try{
+            // parsing a CSV file into BufferedReader class constructor
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            while ((line = br.readLine()) != null) // returns a Boolean value
+            {
+                
+                String[] connection = line.split(splitBy); // use comma as separator
+                
+                Vertex fromVertex = null;
+                Vertex toVertex   = null;
+                boolean from_alreadyInList = true;
+                boolean to_alreadyInList   = true;
+                for (Vertex vertex : vertices) {
+                    if (vertex.name.equals(connection[0])){
+                        fromVertex = vertex;
+                        from_alreadyInList = false;
+                    }
+                    if (vertex.name.equals(connection[1])){
+                        toVertex = vertex;
+                        to_alreadyInList = false;
+                    }
+                }
+                if (from_alreadyInList){
+                    fromVertex = new Vertex(connection[0]);
+                    vertices.add(fromVertex);
+                }
+                if (to_alreadyInList){
+                    toVertex = new Vertex(connection[1]);
+                    vertices.add(toVertex);
+                }
+                if (fromVertex!=null && toVertex!=null) {         
+                    addEdge(fromVertex,toVertex,  Integer.valueOf(connection[2]));
+                    addEdge(toVertex,  fromVertex,Integer.valueOf(connection[2]));
+                }
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();;
+        }
     }
 
     public void PrintGraph() {
@@ -88,61 +125,6 @@ public class AdjacencyGraph {
                 System.out.println(" To: " + currentEdge.to.name + " weight: " + currentEdge.weight);
             }
             System.out.println(" ");
-        }
-    }
-
-
-
-    public void createFromFile(String file) {
-        String line = "";
-        String splitBy = ",";
-        int counter = 0;
-        try {
-            // parsing a CSV file into BufferedReader class constructor
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            while ((line = br.readLine()) != null) // returns a Boolean value
-            {
-                
-                String[] connection = line.split(splitBy); // use comma as separator
-                
-                Vertex tempA = null;
-                Vertex tempB = null;
-                boolean tA = true;
-                boolean tB = true;
-                for (Vertex vertex : vertices) {
-                    if (vertex.name.equals(connection[0])){
-                        tempA = vertex;
-                        tA = false;
-                    }
-                    if (vertex.name.equals(connection[1])){
-                        tempB = vertex;
-                        tB = false;
-                    }
-                }
-                if (tA){
-                    tempA = new Vertex(connection[0]);
-                    vertices.add(tempA);
-                    vertexMap.put(tempA, counter);
-                    counter++;
-                }
-                if (tB){
-                    tempB = new Vertex(connection[1]);
-                    vertices.add(tempB);
-                    vertexMap.put(tempB, counter);
-                    counter++;
-                }
-                if (tempA!=null && tempB!=null) {         
-                    //System.out.println(vertices);
-                    addEdge(tempA,tempB,Integer.valueOf(connection[2]));
-                    addEdge(tempB,tempA,Integer.valueOf(connection[2]));
-                }
-                
-                // connection[[town1, town2, dist],[town1, town2, dist] ]
-                
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
@@ -252,7 +234,7 @@ class Pair implements Comparable<Pair>{
     Integer key;
  
     public Pair(Vertex vertex, Integer distance){
-        //this.distance = vertex.getDist();
+        //this.disance = vertex.getDist();
         this.distance = distance;
         this.vertex = vertex;
     }
